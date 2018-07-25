75c7f8e941e2d9bbfe858246315cf508ede9b80d
MrAnsheff <mransheff86@gmail.com>
2018-07-25 17:29:57 +0400

    With no mistakes

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 class App extends StatelessWidget {
   Widget build(context){
     return MaterialApp(
+      title: 'Log me in!',
       home: Scaffold(
         body: Text('Here will be the form!'),
       ),
-    )
+    );
   }
 }
