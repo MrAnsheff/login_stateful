343f4520ed842f3e174bc43904615e09b06cf750
MrAnsheff <mransheff86@gmail.com>
2018-07-25 17:27:58 +0400

    Null App

---
+++ lib/src/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  Widget build(context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Text('Here will be the form!'),
+      ),
+    )
+  }
+}
